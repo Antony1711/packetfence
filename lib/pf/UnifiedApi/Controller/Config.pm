fb0f42ce622b2d1c2997d922a0314ea4f99db7b8
James Rouzier <jrouzier@inverse.ca>
2026-02-13 16:33:02 +0000

    Don't use Toggle for allowed fields

--- lib/pf/UnifiedApi/Controller/Config.pm
+++ lib/pf/UnifiedApi/Controller/Config.pm
@@ -1158,13 +1158,6 @@ sub field_allowed {
                 $element->_load_options();
                 $allowed = $element->options;
             }
-        } elsif ($field->isa('pfappserver::Form::Field::Toggle')) {
-            my $check = $field->checkbox_value;
-            my $uncheck = $field->unchecked_value;
-            $allowed = [
-                { label => $check, value => $check },
-                { label => $uncheck, value => $uncheck },
-            ];
         }
     }
 
