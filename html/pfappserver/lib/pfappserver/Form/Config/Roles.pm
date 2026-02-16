ec19c0ad059b19c670db593483a11b427a8bdf3f
vparis <codedude@users.noreply.github.com>
2026-02-16 15:29:37 -0500

    replace a lot in lib

--- html/pfappserver/lib/pfappserver/Form/Config/Roles.pm
+++ html/pfappserver/lib/pfappserver/Form/Config/Roles.pm
@@ -32,7 +32,7 @@ has_field 'id' =>
    label => 'Name',
    required => 1,
    messages => { required => 'Please specify a name for the role.' },
-   apply => [ 
+   apply => [
     {
         check => qr/^[a-zA-Z0-9][a-zA-Z0-9_-]*$/,
         message =>
@@ -67,15 +67,11 @@ has_field 'max_nodes_per_pid' =>
 
 has_field 'include_parent_acls' => (
     type => 'Toggle',
-    checkbox_value => 'enabled',
-    unchecked_value => 'disabled',
     label => 'Include parent ACLs',
 );
 
 has_field 'fingerbank_dynamic_access_list' => (
     type => 'Toggle',
-    checkbox_value => 'enabled',
-    unchecked_value => 'disabled',
     label => 'Enabled Fingerbank Dynamic AccessList',
 );
 
@@ -88,25 +84,19 @@ has_field 'acls' => (
 has_field 'inherit_vlan' => (
     type => 'Toggle',
     label => 'Inherit VLAN',
-    checkbox_value => 'enabled',
-    unchecked_value => 'disabled',
-    default => 'disabled',
+    default => 'false',
 );
 
 has_field 'inherit_role' => (
     type => 'Toggle',
     label => 'Inherit Role',
-    checkbox_value => 'enabled',
-    unchecked_value => 'disabled',
-    default => 'disabled',
+    default => 'false',
 );
 
 has_field 'inherit_web_auth_url' => (
     type => 'Toggle',
     label => 'Inherit Web Auth URL',
-    checkbox_value => 'enabled',
-    unchecked_value => 'disabled',
-    default => 'disabled',
+    default => 'false',
 );
 
 =head2 validate
